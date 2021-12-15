ff4e645b3726bac64bf29a428e89e23f85c5c996
TomaVinar <poleetc2017@gmail.com>
2021-12-15 08:20:02 +0200

    Additional lesson 13 JS

---
+++ js_lesson13/additional/script.js
@@ -0,0 +1,40 @@
+// - Імітуємо наповнення інтернет магазину товарами :
+//     Створити форму з наступними полями :
+//     - назва товару
+// - кількість товару
+// - ціна товару
+// - картинка товару (посилання з інтернету)
+
+
+let name = document.getElementById('name');
+let amount = document.getElementById('amount');
+let price = document.getElementById('price');
+let img = document.getElementById('image');
+let button = document.getElementsByTagName('button')[0];
+
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    const product = {
+        id: new Date().getTime(),
+        name: name.value,
+        amount: amount.value,
+        price: price.value,
+        img: img.value
+    }
+
+    let data = localStorage.getItem('product');
+    if (data) {
+        const arr = JSON.parse(data)
+        localStorage.setItem('product', JSON.stringify([...arr, product]));
+    } else {
+        localStorage.setItem('product', JSON.stringify([product]));
+
+    }
+})
+
+
+// Зберігати товари в масив в локалсорадж. При збережені товару з форми, action не повинно відбуватись (preventDefault)
+// створити елемент <a href='list.html'> На сторінку товарів</a>, та list.html, при переході на який відобразити на сторінці всі товари.
+// На сторінці  list.html побудувати кнопку яка видаляє всі товари з корзини та локалстораджа.
+//     До кожного товару додати кнопку, при кліку на яку з лс видаляється конкретний обраний  товар
---
+++ js_lesson13/additional/script_list.js
@@ -0,0 +1,35 @@
+let btn = document.createElement('button');
+btn.innerText = 'Delete All'
+document.body.appendChild(btn);
+
+btn.onclick = (e) => {
+    localStorage.removeItem('product')
+    location.reload()
+}
+
+let array = JSON.parse(localStorage.getItem('product'));
+
+for (const arrayElement of array) {
+    let itemDiv = document.createElement('div');
+    let title = document.createElement('h5');
+    let sum = document.createElement('p');
+    sum.style.fontWeight = 'bold';
+    let itemPrice = document.createElement('div');
+    let itemImage = document.createElement('img');
+    let itemBtn = document.createElement('button');
+    itemBtn.innerText = 'Delete product';
+
+    title.innerText = `${arrayElement.name}`;
+    sum.innerText = `${arrayElement.amount}`;
+    itemPrice.innerText = `${arrayElement.price}`;
+    itemImage.src = `${arrayElement.image}`;
+    itemDiv.append(title, sum, itemPrice, itemImage, itemBtn);
+    document.body.appendChild(itemDiv);
+    itemBtn.onclick = () => {
+       let newArray = array.filter (value => value.id !== arrayElement.id);
+        localStorage.setItem('product', JSON.stringify(newArray));
+        location.reload()
+
+    }
+
+}
